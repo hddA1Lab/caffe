b1abc448f1ee3f278fb6accf8a2f536a24dce770
Zongqing Hou <zongqingh@gmail.com>
2020-04-20 23:40:07 +0800

    mul the bottoms

--- src/caffe/layers/conv_weight_layer.cpp
+++ src/caffe/layers/conv_weight_layer.cpp
@@ -26,7 +26,7 @@ namespace caffe {
         Dtype* top_data = top[0]->mutable_cpu_data();
 
         for(int i = 0; i < this->top_dim_; i++){
-            
+            caffe_mul(1, &bottom[0]->cpu_data()[0], bottom[1]->cpu_data(), &top_data[0]);
         }
     }
 
@@ -37,10 +37,17 @@ namespace caffe {
     }
 
     template<typename Dtype>
+    void ConvolutionWeightLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& top,
+                                                    const vector<Blob<Dtype>*>& bottom){
+
+    }
+
+    template<typename Dtype>
     void ConvolutionWeightLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
                               const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom){
 
     }
+
 
 INSTANTIATE_CLASS(ConvolutionWeightLayer);
 REGISTER_LAYER_CLASS(ConvolutionWeight);
